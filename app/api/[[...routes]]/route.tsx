7ce4884525adca4d892164c1a2e4c377c5bca417
Steve <stevedsimkins@gmail.com>
2024-03-07 17:46:44 -0500

    WIP

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -2,7 +2,44 @@
 
 import { Button, Frog, parseEther } from 'frog'
 import { handle } from 'frog/next'
+import { createWalletClient, http, createPublicClient } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { base } from "viem/chains";
 import abi from './abi.json'
+
+const CONTRACT = `${process.env.CONTRACT_ADDRESS}`
+
+const account = privateKeyToAccount(
+  (process.env.PRIVATE_KEY as `0x`) || "",
+);
+
+export const publicClient = createPublicClient({
+  chain: base,
+  transport: http(process.env.ALCHEMY_URL),
+});
+
+const walletClient = createWalletClient({
+  account,
+  chain: base,
+  transport: http(process.env.ALCHEMY_URL),
+});
+
+async function getAddresForFID(fid: any) {
+  try {
+    const data = await fetch(`https://api.pinata.cloud/v3/farcaster/users/${fid}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.PINATA_JWT}`
+      }
+    })
+    const dataRes = await data.json()
+    const address = dataRes.data.verifications[0]
+    return address
+  } catch (error) {
+    console.log(error)
+    return error
+  }
+}
+
 
 const app = new Frog({
   assetsPath: '/',
@@ -23,7 +60,7 @@ app.frame('/', (c) => {
     ]
   })
 })
- 
+
 app.frame('/finish', (c) => {
   return c.res({
     image: "https://dweb.mypinata.cloud/ipfs/QmZPysm8ZiR9PaNxNGQvqdT2gBjdYsjNskDkZ1vkVs3Tju",
@@ -34,15 +71,31 @@ app.frame('/finish', (c) => {
   })
 })
 
-app.frame('/ad', (c) => {
-  // action goes here
+app.frame('/ad', async (c) => {
+
+  const address = await getAddresForFID(c.frameData?.fid)
+  const { request: mint } = await publicClient.simulateContract({
+    account,
+    address: CONTRACT as `0x`,
+    abi: abi.abi,
+    functionName: "mint",
+    args: [address],
+  });
+  const mintTransaction = await walletClient.writeContract(mint);
+  console.log(mintTransaction);
+
+  const mintReceipt = await publicClient.waitForTransactionReceipt({
+    hash: mintTransaction,
+  });
+  console.log("Mint Status:", mintReceipt.status);
+
   console.log('nft minted')
   return c.res({
     action: '/finish',
-    image: "https://dweb.mypinata.cloud/ipfs/QmaQC9shWhLWQiBuWNa2YGUFARekj5Qm7iCE59H4FzeSi4", 
+    image: "https://dweb.mypinata.cloud/ipfs/QmaQC9shWhLWQiBuWNa2YGUFARekj5Qm7iCE59H4FzeSi4",
     imageAspectRatio: '1:1',
     intents: [
-      <Button.Transaction target="/buy-discount">Buy for 0.0025 ETH</Button.Transaction>,
+      <Button.Transaction target="/buy-discount">Buy for 0.00025 ETH</Button.Transaction>,
     ]
   })
 })
@@ -54,19 +107,19 @@ app.transaction('/buy-discount', (c) => {
     chainId: 'eip155:8453',
     functionName: 'buyHat',
     args: [c.frameData?.fid || 0],
-    to: '0xca2aae6b3cb869e30d0dccf222d507832b7c5b2d',
+    to: CONTRACT as `0x`,
     value: parseEther('0.0025')
   })
 })
 
- 
+
 app.transaction('/buy', (c) => {
   return c.contract({
     abi: abi.abi,
     chainId: 'eip155:8453',
     functionName: 'buyHat',
     args: [c.frameData?.fid || 0],
-    to: '0xca2aae6b3cb869e30d0dccf222d507832b7c5b2d',
+    to: CONTRACT as `0x`,
     value: parseEther('0.005')
   })
 })
