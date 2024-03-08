cc917f5f6485b25433ac1fd38da2bb518a1678ff
Steve <stevedsimkins@gmail.com>
2024-03-08 12:24:11 -0500

    WIP

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -96,6 +96,7 @@ const app = new Frog({
 });
 
 app.use('/ad', fdk.analyticsMiddleware({ frameId: "hats-store", customId: "ad" }))
+app.use('/finish', fdk.analyticsMiddleware({ frameId: "hats-store", customId: "purchased" }))
 
 app.frame("/", async (c) => {
   const balance = await remainingSupply();
